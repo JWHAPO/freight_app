3480768b1523ff74d108c11584de893a08ef851b
JWHAPO <kim2101714@gmail.com>
2019-04-29 23:31:51 +0900

    bind a variable in an order object

--- app/src/main/java/com/bomi/hapo/freight_app/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/bomi/hapo/freight_app/viewmodel/OrderViewModel.kt
@@ -2,8 +2,13 @@ package com.bomi.hapo.freight_app.viewmodel
 
 import android.app.Application
 import android.databinding.BaseObservable
+import android.databinding.Bindable
 import com.bomi.hapo.freight_app.common.network.ApiService
+import com.bomi.hapo.freight_app.model.Order
+import com.bomi.hapo.freight_app.model.OrderResponse
 import io.reactivex.disposables.CompositeDisposable
+import java.time.LocalDate
+import java.time.LocalTime
 
 /**
  *
@@ -14,5 +19,14 @@ class OrderViewModel(private val application: Application) : BaseObservable() {
     private lateinit var apiService: ApiService
     private var mCompositeDisposable: CompositeDisposable = CompositeDisposable()
 
+    var orderResponse: OrderResponse = OrderResponse(0L,0L, LocalDate.now(),LocalTime.now(),0L,0L,"","",0L,"N")
+    var orderResponses : List<OrderResponse> = listOf(orderResponse)
+    @Bindable
+    var order: Order = Order(0L,"",0L,"","",0L, LocalDate.now(), LocalTime.now(),0L,"N","","","",orderResponses)
+
+
+    fun onRequestBtnClick(){
+
+    }
 
 }

59ef47725421507d7cfa647b4c169ff57ce63a8b
JWHAPO <kim2101714@gmail.com>
2019-05-01 22:43:11 +0900

    add getOrder function into OrdeViewModel

--- app/src/main/java/com/bomi/hapo/freight_app/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/bomi/hapo/freight_app/viewmodel/OrderViewModel.kt
@@ -3,10 +3,13 @@ package com.bomi.hapo.freight_app.viewmodel
 import android.app.Application
 import android.databinding.BaseObservable
 import android.databinding.Bindable
+import com.bomi.hapo.freight_app.common.network.ApiClient
 import com.bomi.hapo.freight_app.common.network.ApiService
 import com.bomi.hapo.freight_app.model.Order
 import com.bomi.hapo.freight_app.model.OrderResponse
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -19,14 +22,33 @@ class OrderViewModel(private val application: Application) : BaseObservable() {
     private lateinit var apiService: ApiService
     private var mCompositeDisposable: CompositeDisposable = CompositeDisposable()
 
-    var orderResponse: OrderResponse = OrderResponse(0L,0L, LocalDate.now(),LocalTime.now(),0L,0L,"","",0L,"N")
-    var orderResponses : List<OrderResponse> = listOf(orderResponse)
+    var orderResponse: OrderResponse = OrderResponse(0L, 0L, LocalDate.now(), LocalTime.now(), 0L, 0L, "", "", 0L, "N")
+    var orderResponses: List<OrderResponse> = listOf(orderResponse)
     @Bindable
-    var order: Order = Order(0L,"",0L,"","",0L, LocalDate.now(), LocalTime.now(),0L,"N","","","",orderResponses)
+    var order: Order =
+        Order(0L, "", 0L, "", "", 0L, LocalDate.now(), LocalTime.now(), 0L, "N", "", "", "", orderResponses)
 
 
-    fun onRequestBtnClick(){
+    fun onRequestBtnClick() {
 
+    }
+
+    private fun getOder(order: Order) {
+        apiService = ApiClient.getClient(application).create(ApiService::class.java)
+
+        mCompositeDisposable.add(
+            apiService.getOrder(order.orderId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::successGetOrder, this::failGetOrder)
+        )
+    }
+
+    private fun successGetOrder(order: Order) {
+
+    }
+    private fun failGetOrder(error: Throwable) {
+        println("error: $error")
     }
 
 }

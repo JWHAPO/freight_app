f0905717798d7643a07f74c3827a9d89b73b0f58
JWHAPO <kim2101714@gmail.com>
2019-05-07 22:33:28 +0900

    add Car model

---
+++ app/src/main/java/com/bomi/hapo/freight_app/model/Car.kt
@@ -0,0 +1,24 @@
+package com.bomi.hapo.freight_app.model
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
+
+/**
+ *
+ * Created by JWHAPO
+ * -19. 4. 18 오후 10:36
+ */
+data class Car(
+    var carId: Long
+    , var carNo: Long
+    , var description: String
+    , var width: Long
+    , var widthUom: String
+    , var length: Long
+    , var lengthUom: String
+    , var loadableHeight: Long
+    , var loadableHeightUom: String
+    , var loadableWeight: Long
+    , var loadableWeightUom: String
+) : Observable()
